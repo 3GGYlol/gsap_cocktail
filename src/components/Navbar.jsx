c6ebad1c19c18acc0cc544ba2acb2f582038032e
3GGYlol <xlj135@gmail.com>
2025-09-11 14:46:55 -0400

    yay me

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+       <nav>
+           <div>
+               <a href="#home" className="flex items-center gap-2">
+                   <p>Bleeding Heart</p>
+               </a>
+
+               <ul>
+                   {}
+               </ul>
+           </div>
+       </nav>
+    )
+}
+export default Navbar;
