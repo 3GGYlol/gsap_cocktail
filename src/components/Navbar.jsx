5a8cd8fab57dda871f1c688f2aff72e59b5a6bcc
3GGylol <xlj135@gmail.com>
2026-02-19 12:55:17 -0500

    Nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,30 @@
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react'
+
 import {navLinks} from "../../constants/index.js";
 
 const Navbar = () => {
+    useGSAP(() => {
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'nav',
+                start: 'bottom top'
+            }
+        });
+
+        navTween.fromTo('nav', {backgroundColor: 'transparent'}, {
+         backgroundColor: '#0000050',
+             backgroundFilter: 'blur(10px)',
+             duration: 1,
+             ease: 'power1.inOut'
+        });
+    })
+
     return (
        <nav>
            <div>
                <a href="#home" className="flex items-center gap-2">
+                  <img src="/images/logo.png" alt="logo" />
                    <p>Bleeding Heart</p>
                </a>
 
