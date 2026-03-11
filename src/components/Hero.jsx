a1c69bf62aecfa7e650e3c2501b59108939103cf
3GGylol <xlj135@gmail.com>
2026-03-11 10:23:28 -0400

    adding animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,15 @@
 import {useGSAP} from "@gsap/react";
 import gsap from 'gsap';
 import {SplitText} from 'gsap/all'
+import {useRef} from 'react';
+import {useMediaQuery} from 'react-responsive';
 
 
 const Hero = () => {
+    const videoRef = useRef();
+
+    const isMobile = useMediaQuery({ maxWidth: 800 });
+
     useGSAP(() => {
      const heroSplit = new SplitText('.title', {type: 'chars, words' });
      const paragraphSplit = new SplitText('.subtitle', {type: 'lines' });
@@ -35,7 +41,11 @@ const Hero = () => {
      })
          .to('.right-leaf', { y: 200 }, 0)
         .to('.left-leaf', { y: -200 }, 0)
-    }, []);
+
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
+
+        }, []);
     
     return (
         <>
@@ -73,6 +83,16 @@ const Hero = () => {
                    </div>
                 </div>
         </section>
+
+            <div className="video absolute inset-0">
+             <video
+             ref={videoRef}
+             src="/videos/input.mp4"
+             muted
+             playsInline
+             preload="auto"
+             />
+            </div>
         </>
     )
 }
